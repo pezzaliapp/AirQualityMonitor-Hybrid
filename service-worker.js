bf48a6ff8a54dd9ed2b8b8461644a7bc174f062d
pezzaliapp <pezzalialessandro@gmail.com>
2025-04-06 19:40:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,13 @@
 self.addEventListener("install", e => {
-  console.log("Service Worker installed");
-  e.waitUntil(self.skipWaiting());
+  e.waitUntil(
+    caches.open("aqm-cache").then(cache =>
+      cache.addAll(["./", "index.html", "style.css", "app.js"])
+    )
+  );
 });
 
-self.addEventListener("activate", e => {
-  console.log("Service Worker activated");
-  e.waitUntil(self.clients.claim());
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
